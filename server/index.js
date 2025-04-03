c546472fb81e945663fb70d8dc736fe93a17529b
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2025-04-04 01:18:18 +0530

    fixed server js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
-import cors from "cors";
-import connectDB from "./mongodb/connect.js";
+//import cors from "cors";
+//import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/geminiroutes.js";
 import express from "express";
 
 const app = express();
-app.use(cors());
+const PORT = 4000;
+//app.use(cors());
 //app.use(json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
@@ -16,7 +17,7 @@ app.get("/", (req, res) => {
     .status(200)
     .json("Hello Aayush, Cloud Backend Server is running successfully");
 });
-connectDB(process.env.MONGODB_URL);
+//connectDB(process.env.MONGODB_URL);
 app.listen(process.env.PORT || 5000, () => console.log("Server started"));
 
 module.exports = app;
