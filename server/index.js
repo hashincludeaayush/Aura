27b1c90409501618817634e2339f500d47744a73
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2025-04-04 00:51:54 +0530

    fixed server js

--- server/index.js
+++ server/index.js
@@ -15,15 +15,12 @@ app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/gemini', dalleRoutes);
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
     message: 'Hello Aayush, Cloud Backend Server is running successfully',
   });
 });
+connectDB(process.env.MONGODB_URL);
+app.listen(process.env.PORT || 8080, () => console.log('Server started'));
 
-try {
-  connectDB(process.env.MONGODB_URL);
-  app.listen(process.env.PORT || 8080, () => console.log('Server started'));
-} catch (error) {
-  console.log(error);
-}
+module.exports = app;
