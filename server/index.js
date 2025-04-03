2bc6004977bcf9508b87b54d1e65a5452190d713
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2025-04-04 01:00:21 +0530

    fixed server js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/geminiroutes.js';
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/geminiroutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/gemini', dalleRoutes);
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/gemini", dalleRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).json('Hello Aayush, Cloud Backend Server is running successfully');
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json("Hello Aayush, Cloud Backend Server is running successfully");
 });
 connectDB(process.env.MONGODB_URL);
-app.listen(process.env.PORT || 8080, () => console.log('Server started'));
-
-module.exports = app;
+app.listen(process.env.PORT || 8080, () => console.log("Server started"));
