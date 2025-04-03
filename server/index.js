83cb93e4b22b49b05a7704670781db132869e3b4
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2025-04-04 01:11:53 +0530

    fixed server js

--- server/index.js
+++ server/index.js
@@ -1,16 +1,12 @@
-import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
-
 import connectDB from "./mongodb/connect.js";
 import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/geminiroutes.js";
-
-dotenv.config();
+import express from "express";
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
+//app.use(json({ limit: "50mb" }));
 
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/gemini", dalleRoutes);
